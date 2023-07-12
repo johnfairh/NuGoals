bd343eb6c12a7806fee6c89d58b2f80131da545d
John Fairhurst <johnfairh@gmail.com>
2023-07-12 13:17:37 +0100

    Cleaned-up Alarm model

--- NuGoals/Model/Alarm.swift
+++ NuGoals/Model/Alarm.swift
@@ -8,24 +8,22 @@ import SwiftData
 
 @Model
 final public class Alarm {
-    var cdNextActiveDate: Date
-    var cdType: Int16
-    var cdWeekDay: Int16
+    @Attribute(originalName: "cdNextActiveDate") var nextActiveDate: Date
+    var cdType: Int16    // XXX
+    var cdWeekDay: Int16 // XXX turn into enum
     var name: String
     var notificationUid: String?
-    @Transient var sectionOrder: String?
     var sortOrder: Int64
     @Relationship(.cascade, inverse: \Note.activeAlarm) var activeNote: Note?
     @Relationship(.cascade, inverse: \Note.defaultAlarm) var defaultNote: Note?
     @Relationship var icon: Icon
 
     init() {
-        cdNextActiveDate = .distantFuture
+        nextActiveDate = .distantFuture
         cdType = 0
         cdWeekDay = 1
         name = ""
         notificationUid = nil
-        sectionOrder = nil
         sortOrder = 0
     }
 }
